700e5f5633f507dd230016b28014fea116ddcccf
benaime07 <benaime007@gmail.com>
2026-07-29 11:54:31 +0200

    Added database config, logo and updated navbar

--- Masaka-MMS/app.py
+++ Masaka-MMS/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
@@ -8,4 +13,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
